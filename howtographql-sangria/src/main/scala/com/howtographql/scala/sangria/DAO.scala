3d383e4230ed3300b2eddc395e037c5c113c5ec0
Zi Jing <bigzijing@gmail.com>
2020-06-04 17:57:32 +0800

    7: Relations

--- howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/DAO.scala
+++ howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/DAO.scala
@@ -3,6 +3,7 @@ package com.howtographql.scala.sangria
 import DBSchema._
 import com.howtographql.scala.sangria.models.{Link, User, Vote}
 import slick.jdbc.H2Profile.api._
+import sangria.execution.deferred.{RelationIds, SimpleRelation}
 
 import scala.concurrent.Future
 
@@ -22,4 +23,27 @@ class DAO(db: Database) {
       Votes.filter(_.id inSet ids).result
     )
   }
+
+  def getLinksByUserIds(ids: Seq[Int]): Future[Seq[Link]] = {
+    db.run {
+      Links.filter(_.postedBy inSet ids).result
+    }
+  }
+
+  def getVotesByUserIds(ids: Seq[Int]): Future[Seq[Vote]] = {
+    db.run {
+      Votes.filter(_.userId inSet ids).result
+    }
+  }
+
+  def getVotesByRelationIds(rel: RelationIds[Vote]): Future[Seq[Vote]] =
+    db.run(
+      Votes.filter { vote =>
+        rel.rawIds.collect({
+          case (SimpleRelation("byUser"), ids: Seq[Int]) => vote.userId inSet ids
+          case (SimpleRelation("byLink"), ids: Seq[Int]) => vote.linkId inSet ids
+        }).foldLeft(true: Rep[Boolean])(_ || _)
+
+      } result
+    )
 }
--- howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/DBSchema.scala
+++ howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/DBSchema.scala
@@ -10,6 +10,7 @@ import scala.concurrent.duration._
 import scala.concurrent.Await
 import scala.language.postfixOps
 
+
 object DBSchema {
 
   implicit val dateTimeColumnType = MappedColumnType.base[DateTime, Timestamp](
@@ -17,14 +18,18 @@ object DBSchema {
     ts => DateTime(ts.getTime)
   )
 
+
   class LinksTable(tag: Tag) extends Table[Link](tag, "LINKS"){
 
     def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
     def url = column[String]("URL")
     def description = column[String]("DESCRIPTION")
+    def postedBy = column[Int]("USER_ID")
     def createdAt = column[DateTime]("CREATED_AT")
 
-    def * = (id, url, description, createdAt).mapTo[Link]
+    def * = (id, url, description, postedBy, createdAt).mapTo[Link]
+
+    def postedByFK = foreignKey("postedBy_FK", postedBy, Users)(_.id)
 
   }
 
@@ -49,34 +54,41 @@ object DBSchema {
     def createdAt = column[DateTime]("CREATED_AT")
 
     def * = (id, userId, linkId, createdAt).mapTo[Vote]
+
+    def userFK = foreignKey("user_FK", userId, Users)(_.id)
+    def linkFK = foreignKey("link_FK", linkId, Links)(_.id)
   }
 
   val Votes = TableQuery[VotesTable]
 
   /**
-   * Load schema and populate sample data withing this Sequence od DBActions
+   * Load schema and populate sample data within this Sequence od DBActions
    */
   val databaseSetup = DBIO.seq(
-    Links.schema.create,
     Users.schema.create,
+    Links.schema.create,
     Votes.schema.create,
-    Links forceInsertAll Seq(
-      Link(1, "http://howtographql.com", "Awesome community driven GraphQL tutorial", DateTime(2017,9,12)),
-      Link(2, "http://graphql.org", "Official GraphQL web page",DateTime(2017,10,1)),
-      Link(3, "https://facebook.github.io/graphql/", "GraphQL specification",DateTime(2017,10,2))
-    ),
+
     Users forceInsertAll Seq(
       User(1, "mario", "mario@example.com", "s3cr3t"),
       User(2, "Fred", "fred@flinstones.com", "wilmalove")
     ),
-    Votes forceInsertAll Seq(
-      Vote(id = 1, userId = 1, linkId = 1),
-      Vote(id = 2, userId = 1, linkId = 2),
-      Vote(id = 3, userId = 1, linkId = 3),
-      Vote(id = 4, userId = 2, linkId = 2),
-    )
 
+    Links forceInsertAll Seq(
+      Link(1, "http://howtographql.com", "Awesome community driven GraphQL tutorial",1, DateTime(2017,9,12)),
+      Link(2, "http://graphql.org", "Official GraphQL web page",1, DateTime(2017,10,1)),
+      Link(3, "https://facebook.github.io/graphql/", "GraphQL specification",2, DateTime(2017,10,2))
+    ),
+
+    Votes forceInsertAll Seq(
+      Vote(1, 1, 1),
+      Vote(2, 1, 2),
+      Vote(3, 1, 3),
+      Vote(4, 2, 2),
+    )
   )
+
+
 
   def createDatabase: DAO = {
     val db = Database.forConfig("h2mem")
@@ -84,6 +96,7 @@ object DBSchema {
     Await.result(db.run(databaseSetup), 10 seconds)
 
     new DAO(db)
+
   }
 
 }
