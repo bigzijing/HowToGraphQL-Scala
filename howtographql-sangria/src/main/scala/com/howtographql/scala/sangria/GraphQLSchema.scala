f7c84c3a0744ccd1150858b85e7240e61a21f9d0
Zi Jing <bigzijing@gmail.com>
2020-06-04 16:06:11 +0800

    5: Custom Scalars

--- howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
+++ howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
@@ -1,26 +1,44 @@
 package com.howtographql.scala.sangria
 
+import akka.http.scaladsl.model.DateTime
 import sangria.schema.{ListType, ObjectType}
 import models._
+import sangria.ast.StringValue
+import sangria.execution.deferred.{DeferredResolver, Fetcher, HasId}
 import sangria.schema._
 import sangria.macros.derive._
-import sangria.execution.deferred.Fetcher
-import sangria.execution.deferred.DeferredResolver
-import sangria.execution.deferred.HasId
 
 object GraphQLSchema {
 
-  implicit val LinkType = deriveObjectType[Unit, Link]()
-  implicit val linkHasId = HasId[Link, Int](_.id)
+  implicit val GraphQLDateTime = ScalarType[DateTime](//1
+    "DateTime",//2
+    coerceOutput = (dt, _) => dt.toString, //3
+    coerceInput = { //4
+      case StringValue(dt, _, _ ) => DateTime.fromIsoDateTimeString(dt).toRight(DateTimeCoerceViolation)
+      case _ => Left(DateTimeCoerceViolation)
+    },
+    coerceUserInput = { //5
+      case s: String => DateTime.fromIsoDateTimeString(s).toRight(DateTimeCoerceViolation)
+      case _ => Left(DateTimeCoerceViolation)
+    }
+  )
 
-  val Id = Argument("id", IntType)
-  val Ids = Argument("ids", ListInputType(IntType))
+
+  val LinkType = deriveObjectType[Unit, Link](
+    ReplaceField("createdAt", Field("createdAt", GraphQLDateTime, resolve = _.value.createdAt))
+  )
+
+  implicit val linkHasId = HasId[Link, Int](_.id)
 
   val linksFetcher = Fetcher(
     (ctx: MyContext, ids: Seq[Int]) => ctx.dao.getLinks(ids)
   )
+
   val Resolver = DeferredResolver.fetchers(linksFetcher)
 
+
+  val Id = Argument("id", IntType)
+  val Ids = Argument("ids", ListInputType(IntType))
 
   val QueryType = ObjectType(
     "Query",
@@ -33,7 +51,7 @@ object GraphQLSchema {
       ),
       Field("links",
         ListType(LinkType),
-        arguments = List(Argument("ids", ListInputType(IntType))),
+        arguments = Ids :: Nil,
         resolve = c => linksFetcher.deferSeq(c.arg(Ids))
       )
     )
